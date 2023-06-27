14b7f4814678bfb8c6af1c3fb7183b3888c2f507
Matt Osborne <MattJOzzy@gmail.com>
2023-06-27 12:18:44 -0500

    integrated google maps API

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,20 +1,31 @@
-import { Box, Button, IconButton, Typography, useTheme } from "@mui/material";
+import { Box, useTheme } from "@mui/material";
 import { tokens } from "../../theme";
-import Header from "../../components/Header";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: 41.881832,
+  lng: -87.623177,
+};
 
 const Dashboard = () => {
-    const theme = useTheme();
-    const colors = tokens(theme.palette.mode);
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
 
-    return (
-        <Box m="20px">
-            <Box display="flex" justifyContent="space-between" alignItems="center">
-
-            {/* insert map here */}
-
-            </Box>
-        </Box>
-    );
+  return (
+    <Box>
+      <LoadScript googleMapsApiKey="AIzaSyCzO6Fxi3JJdhu5jBl5VsgrPV473-8Rw7M">
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+          {/* Child components, such as markers, info windows, etc. */}
+          <></>
+        </GoogleMap>
+      </LoadScript>
+    </Box>
+  );
 };
 
 export default Dashboard;
